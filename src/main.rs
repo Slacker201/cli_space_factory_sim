2d4f8d9b37d63f8d643af5e7da2a221690403ba8
Slacker201 <slacker1234567809@gmail.com>
2025-07-07 00:30:46 -0600

    this project is unusable without hardcoding the gameloop. pls wait about 2 years for a decent game(provided I stay focused)

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@
 mod item_utils;
 mod entities;
 pub fn main() {
-    println!("Running")
+    println!("why tf are u running this. This project is nowhere near a runnable state. Try factorio or something if ur this bored")
 }
