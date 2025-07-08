56b916cd5871feaf7cbe7ed8987d9ff91e9f49e6
Slacker201 <slacker1234567809@gmail.com>
2025-07-07 22:46:13 -0600

    Added documentation

--- src/entities/entity_components/inventory/inventory.rs
+++ src/entities/entity_components/inventory/inventory.rs
@@ -7,24 +7,34 @@ use crate::item_utils::{item::item::Item, transport_order::transport_order::Tran
 
 
 #[derive(Debug, PartialEq)]
+/// Represents an inventory that holds items. Used by objects to manage their item collections.
 pub struct Inventory {
+    /// The item hashmap which stores items indexed by their unique item id
     items: HashMap<u64, Item>,
+    /// The max weight capacity of the inventory. Currently unimplemented
     max_capacity: u64,
+    /// Indicates whether the items have changed, used to avoid unnecessary recalculations of capacity.
     items_changed: bool,
+    /// Cached total item count
     capacity: u128
 }
 
 
 impl Inventory {
+    /// Generates a new empty `Inventory` with a maximum capacity of 100.
     pub fn new() -> Inventory {
         Inventory { items: HashMap::new(), max_capacity: 100, items_changed: true, capacity: 0}
     }
+    /// Returns a reference to the items hashmap
     pub fn items(&self) -> &HashMap<u64, Item> {
         &self.items
     }
+    /// Returns the max capacity
     pub fn max_capacity(&self) -> u64 {
         self.max_capacity
     }
+    /// Adds an item to the hashmap, adding to the item count if the item is already in the map
+    /// Currently does not respect max capacity.
     pub fn add(&mut self, item_to_add: Item) -> (bool, Option<Item>) {
         let id = item_to_add.id();
         self.items_changed = true;
@@ -39,41 +49,50 @@ impl Inventory {
             },
         }
     }
-
+    /// returns an optional reference to the item corresponding to the id
     pub fn get(&self, id: u64) -> Option<&Item> {
         self.items.get(&id)
     }
+    /// returns a mutable option to the item corresponding to the id
     pub fn get_mut(&mut self, id: u64) -> Option<&mut Item> {
         self.items_changed = true;
         self.items.get_mut(&id)
     }
-
+    /// Returns the capacity
     pub fn capacity(&mut self) -> u128 {
         if self.items_changed {
             let mut mass_count = 0u128;
             for item in &self.items {
-                mass_count += item.1.count();
+                mass_count += item.1.count()
             }
             self.capacity = mass_count;
         }
         self.items_changed = false;
         self.capacity
     }
-
+    /// Removes an item from the inventory and returns it if found
     pub fn remove(&mut self, item: &Item) -> Option<Item> {
         self.items.remove(&item.id())
     }
+    /// Removes an item by id, and returns it if found
     pub fn remove_by_id(&mut self, id: u64) -> Option<Item> {
         self.items.remove(&id)
     }
+    /// Sets the maximum capacity.
+    /// No checks are conducted
     pub fn set_max_capacity(&mut self, new_capacity: u64) {
         self.max_capacity = new_capacity;
     }
-
+    /// Returns a vector of references to all items in the inventory
     pub fn get_all(&self) -> Vec<&Item> {
         self.items.values().collect()
     }
+    /// Returns a vector of mutable references to all items in the inventory
+    pub fn get_all_mut(&mut self) -> Vec<&mut Item> {
+        self.items.values_mut().collect()
+    }
 
+    /// Moves items from itself into another inventory according to a transport order
     pub fn move_items_to(&mut self, t_order: TransportOrder, tar_inv: &mut Inventory) {
         println!("Currently adding these items: {:?}", t_order.items());
         for item in t_order.items() {
@@ -95,16 +114,15 @@ impl Inventory {
             }
         }
     }
-
+    /// Adds multiple items to the inventory
     pub fn add_multiple(&mut self, items: Vec<Item>) {
         for item in items {
             self.add(item);
         }
     }
-
+    /// Clears the inventory of all items
     pub fn clear(&mut self) {
         self.items.clear();
     }
-
 }
 
