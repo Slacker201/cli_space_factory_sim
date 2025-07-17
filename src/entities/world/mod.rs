7b56f9da64a46d84deeefed679ee9941250d8d19
Slacker201 <slacker1234567809@gmail.com>
2025-07-16 22:40:35 -0600

    shut off a warning

--- src/entities/world/mod.rs
+++ src/entities/world/mod.rs
@@ -8,7 +8,7 @@ use crate::{entities::node::Node, item_utils::recipe::recipe::Recipe};
 
 
 pub struct World {
-    player_recipes: HashMap<String, Recipe>,
+    _player_recipes: HashMap<String, Recipe>,
     all_recipes: HashMap<String, Recipe>,
     node: Node
 }
@@ -16,7 +16,7 @@ pub struct World {
 
 impl World {
     pub fn new() -> World {
-        World { player_recipes: HashMap::new(), all_recipes: HashMap::new(), node: Node::new() }
+        World { _player_recipes: HashMap::new(), all_recipes: HashMap::new(), node: Node::new() }
     }
     pub fn all_recipes_mut(&mut self) -> &mut HashMap<String, Recipe> {
         &mut self.all_recipes
