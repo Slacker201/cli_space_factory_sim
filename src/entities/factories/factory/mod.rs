0660b4299fff80d5a4ca4ef700bfb56e2bc393a3
Slacker201 <slacker1234567809@gmail.com>
2025-07-16 22:01:16 -0600

    moved factory code to mod file In AcCoRdAnCe To CoRrEcT pRaCtIcEs

--- src/entities/factories/factory/mod.rs
+++ src/entities/factories/factory/mod.rs
@@ -1,1 +1,48 @@
-pub mod factory;
+
+
+use crate::{
+    entities::{
+        entity_components::{assembler::assembler::Assembler, inventory::Inventory},
+        factories::entity_base::entity_base::EntityBase,
+    },
+    item_utils::transport_order::transport_order::TransportOrder,
+};
+#[derive(Clone)]
+pub struct Factory {
+    assembler: Assembler,
+    id: u64
+}
+
+impl Factory {
+    pub fn new() -> Factory {
+        Factory {
+            assembler: Assembler::new(),
+            id: 0
+        }
+    }
+    pub fn move_items_from_output_to(&mut self, tar_inv: &mut Inventory, t_order: TransportOrder) {
+        self.assembler
+            .output_inventory_mut()
+            .move_items_to(t_order, tar_inv);
+    }
+    pub fn move_items_from_input_to(&mut self, tar_inv: &mut Inventory, t_order: TransportOrder) {
+        self.assembler
+            .input_inventory_mut()
+            .move_items_to(t_order, tar_inv);
+    }
+    pub fn get_assembler(&self) -> &Assembler {
+        &self.assembler
+    }
+    pub fn get_assembler_mut(&mut self) -> &mut Assembler {
+        &mut self.assembler
+    }
+    pub fn id(&self) -> u64 {
+        self.id
+    }
+}
+
+impl EntityBase for Factory {
+    fn tick(&mut self) {
+        self.assembler.tick();
+    }
+}
