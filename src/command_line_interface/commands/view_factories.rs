ed10298e3807bb04299dc419d221d86de07d665d
Slacker201 <slacker1234567809@gmail.com>
2025-07-17 20:59:00 -0600

    Documented view_factories

--- src/command_line_interface/commands/view_factories.rs
+++ src/command_line_interface/commands/view_factories.rs
@@ -1,7 +1,12 @@
 use crate::{command_line_interface::command_struct::Command, entities::world::World};
 
 
-
+/// Prints each factory on a new line
+/// 
+/// # Arguments
+/// * _cmd - The command object constructed by the parser. Unused
+/// * world - A reference to the global world object
+/// 
 pub fn view_factories_cmd (_cmd: Command, world: &World) {
     for fac in world.node().factories().values() {
         println!("{:?}", fac);
